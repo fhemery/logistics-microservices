f2a392d367570a491e8691c7ec4350497ef8bb69
fhemery <f.hemery@hemit.fr>
2023-03-03 11:23:49 +0100

    Improve view so that it works even with stock offline

--- apps/front/src/components/shopping-catalog/shopping-catalog.service.ts
+++ apps/front/src/components/shopping-catalog/shopping-catalog.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
-import { AddToBasketDto, BasketDto, ProductDto } from "@log/contracts";
-import { catchError, map, Observable, of, take } from "rxjs";
+import { AddToBasketDto, BasketDto, ProductDto, StockProductDto } from "@log/contracts";
+import { catchError, map, Observable, of, switchMap, take } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 
 export interface ProductWithStockDto extends ProductDto {
@@ -8,15 +8,25 @@ export interface ProductWithStockDto extends ProductDto {
 }
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root"
 })
 export class ShoppingCatalogService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+  }
 
   getAllAvailableProducts(): Observable<ProductWithStockDto[]> {
     return this.httpClient
-      .get<ProductDto[]>('/api/referential/products')
-      .pipe(map((products) => products.map((p) => ({ ...p, stock: 0 }))));
+      .get<ProductDto[]>("/api/referential/products")
+      .pipe(switchMap((products) => {
+        const productsToReturn: ProductWithStockDto[] = products.map((p) => ({ ...p, stock: 0 }));
+
+        return this.httpClient.get<StockProductDto[]>("/api/stock/stock").pipe(map(stocks => {
+          productsToReturn.forEach(p =>
+            p.stock = stocks.find((s) => s.productId === p._id)?.quantity || 0
+          );
+          return productsToReturn;
+        }), catchError(() => of(productsToReturn)));
+      }));
     /*return forkJoin([
       this.httpClient.get<ProductDto[]>('/api/referential/products'),
       this.httpClient.get<StockProductDto[]>('/api/stock/stock'),
@@ -32,34 +42,34 @@ export class ShoppingCatalogService {
 
   addToCart(_id?: string): Observable<void> {
     return this.httpClient
-      .put<void>('/api/shopping/basket', {
+      .put<void>("/api/shopping/basket", {
         id: _id,
-        quantity: 1,
+        quantity: 1
       } as AddToBasketDto)
       .pipe(
         catchError((e) => {
-          console.log('error during cart add', e);
+          console.log("error during cart add", e);
           return of();
         })
       );
   }
 
   getCart(): Observable<BasketDto> {
-    return this.httpClient.get<BasketDto>('/api/shopping/basket');
+    return this.httpClient.get<BasketDto>("/api/shopping/basket");
   }
 
   checkout() {
     this.httpClient
-      .post('/api/shopping/basket/checkout', {})
+      .post("/api/shopping/basket/checkout", {})
       .pipe(
         take(1),
         catchError((e) => {
-          console.log('error during checkout', e);
+          console.log("error during checkout", e);
           return of(null);
         })
       )
       .subscribe((id) => {
-        console.log('checkout successful with order id', id);
+        console.log("checkout successful with order id", id);
       });
   }
 }
